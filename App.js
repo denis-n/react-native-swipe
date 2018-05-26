4ee79aca46c38bffa6a51f375de4f50b2f6e3b85
qi <12meridiansmedia@gmail.com>
2018-05-26 23:52:36 +0300

    Install react-native-elements.

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Card, Button } from "react-native-elements";
+
+import Data from "./Data";
+import Deck from "./src/Deck";
 
 export default class App extends React.Component {
+  renderCard = item => {
+    // return <Text key={item.id}>{item.text}</Text>;
+    return (
+      <Card key={item.id} title={item.text} image={{ uri: item.uri }}>
+        <Text style={{ marginBottom: 10 }}>
+          I can customize the Card further.
+        </Text>
+        <Button
+          icon={{ name: "code" }}
+          backgroundColor="#03A9F4"
+          title="View Now!"
+        />
+      </Card>
+    );
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Deck data={Data} renderCard={this.renderCard} />
       </View>
     );
   }
@@ -14,8 +34,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff"
+  }
 });
